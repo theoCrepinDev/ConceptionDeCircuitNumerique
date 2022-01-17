9b9fdcb3c881746f8c93be4493eef295fa76f69b
theoCrepinDev <theo.crepin8@gmail.com>
2022-01-17 17:54:53 +0100

    version du 17

--- Code/CMDBUFFERS.vhd
+++ Code/CMDBUFFERS.vhd
@@ -1,6 +1,10 @@
 entity CMDBUFFERS is 
 port(
-
+    SEL_OUT : in STD_LOGIC_VECTOR(1 downto 0);
+    MEM_CACHE_1 : in STD_LOGIC_VECTOR(7 downto 0);
+    MEM_CACHE_2 : in STD_LOGIC_VECTOR(7 downto 0);
+    s : in STD_LOGIC_VECTOR(7 downto 0);
+    RES_OUT : out STD_LOGIC_VECTOR(7 downto 0);
 );
 end CMDBUFFERS
 
